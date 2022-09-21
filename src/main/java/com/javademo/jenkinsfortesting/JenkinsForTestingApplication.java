04f06569515d83bee52164ca75e7ed685c31274b
Hoang Le <le.dinhhoang.1207@outlook.com>
2022-09-21 19:42:07 +0700

    init

---
+++ src/main/java/com/javademo/jenkinsfortesting/JenkinsForTestingApplication.java
@@ -0,0 +1,13 @@
+package com.javademo.jenkinsfortesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsForTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsForTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javademo/jenkinsfortesting/JenkinsForTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javademo.jenkinsfortesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsForTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
