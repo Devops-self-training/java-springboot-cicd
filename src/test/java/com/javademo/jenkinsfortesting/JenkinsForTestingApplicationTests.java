f71a786be38b0c3783e3addb45c51ef74b866a1b
Hoang D. Le <45985646+akakshuki@users.noreply.github.com>
2022-09-25 16:20:33 +0000

    add unit test

--- src/test/java/com/javademo/jenkinsfortesting/JenkinsForTestingApplicationTests.java
+++ src/test/java/com/javademo/jenkinsfortesting/JenkinsForTestingApplicationTests.java
@@ -1,6 +1,7 @@
 package com.javademo.jenkinsfortesting;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,9 @@ class JenkinsForTestingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void checking() {
+		String str = "Junit is working fine";
+		assertEquals("Junit is working fine",str);  
+	}
 }
